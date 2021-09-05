5cd3f39eeaf1e8aafab94cbbab734f41eb5c314c
OLEKSANDR TYMOSHENKO <olexander.tymoshenko@gmail.com>
2021-09-05 13:15:37 +0300

    правиви код

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,4 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
-
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-    mobileMenuRef.classList.toggle("is-open");
-  });
-})();
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
